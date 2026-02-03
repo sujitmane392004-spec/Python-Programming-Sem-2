45d57a6d5d6593c1b836b7c3c722a1b748c8bc17
Python Programming Sem 2 <sujitmane392004@gmail.com>
2026-02-03 11:15:54 +0530

    Create Exp1 Activie.py

---
+++ Exp1/Exp1 Activie.py
@@ -0,0 +1,14 @@
+num1 =int( input("Enter first num:"))
+num2 =int(input("Enter secound num:"))
+ 
+sum=num1+num2
+print("Addition=",sum)
+
+sub=num1-num2
+print("substract=",sub)
+
+multiply=num1*num2
+print("multiplycation=",multiply)
+
+divide=num1/num2
+print("division=",divide)
