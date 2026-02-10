69e27d45221d0b2cf80ee0a21dfae466338f0c2a
Python Programming Sem 2 <sujitmane392004@gmail.com>
2026-02-10 11:24:35 +0530

    Create Exp2Act3.py

---
+++ Exp2/Exp2Act3.py
@@ -0,0 +1,11 @@
+# speed limit
+"""
+Created on Tue Feb 10 10:32:47 2026
+
+@author: sujit manee
+"""
+speed = int(input("Enter speed km/h:"))
+if speed > 60:
+    print("overspeeding!Fine issued")
+else:
+    print("with in limit")
