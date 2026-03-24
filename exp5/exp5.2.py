51d497974679140c466af0feb25e05dd0783b799
Python Programming Sem 2 <sujitmane392004@gmail.com>
2026-03-24 10:34:35 +0530

    Create exp5.2.py

---
+++ exp5/exp5.2.py
@@ -0,0 +1,11 @@
+#  Roll numbers of students in two classes
+"""
+Created on Tue Mar 24 10:32:05 2026
+
+@author: sujit
+"""
+
+class_A = {1, 2, 3, 4, 5}
+class_B = {4, 5, 6, 7, 8}
+common_students = class_A.intersection(class_B)
+print("Students present in both classes:", common_students)
