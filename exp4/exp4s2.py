2f2c30aee3dc8ce8183f7884f1276287452afd83
Python Programming Sem 2 <sujitmane392004@gmail.com>
2026-03-24 10:45:05 +0530

    Create exp4s2.py

---
+++ exp4/exp4s2.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 24 10:42:45 2026
+
+@author: sujit
+"""
+string = input("Enter a string: ")
+reversed_string = string[::-1]
+print("Reversed string:", reversed_string)
